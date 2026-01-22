e260f28b4e5ca6753ad9faaa4bcb7ef203b0cc6b
Anna Kolyagina <kolyagina.ann@gmail.com>
2026-01-22 18:51:19 +0100

    HW25upd

--- tests/mock-tests/mock-tests.hw25.spec.ts
+++ tests/mock-tests/mock-tests.hw25.spec.ts
@@ -5,64 +5,94 @@
 
 import { test, expect } from '@playwright/test';
 
-test('check that page is loaded successfully', async ({ page }) => {
-  const response1 = await page.request.get('https://pu5hds6usi.execute-api.us-east-1.amazonaws.com/mocks');
-  await expect(response1).toBeOK();
-});
+const Base_Url = 'https://pu5hds6usi.execute-api.us-east-1.amazonaws.com/mocks';
 
-test('check html page title', async ({ page }) => {
-  await page.goto('https://pu5hds6usi.execute-api.us-east-1.amazonaws.com/mocks');
-  await expect(page).toHaveTitle(/QA Practice: Mocking/);
-});
+const API_Pattern = '**/*?action=getData';//Это шаблон запроса, который мы будем ловить, запрос всегда заканчивается на ?action=getData, **/* — значит «любой домен и любой путь»
 
-test('check h3 title text', async ({ page }) => {
-  await page.goto('https://pu5hds6usi.execute-api.us-east-1.amazonaws.com/mocks');  
-  const h3Title = await page.locator('h3').textContent();
-  expect(h3Title).toBe('Mock trainer');
-});
+test.describe('API mocking', () => {
 
-test('check success message text', async ({ page }) => {
-  await page.goto('https://pu5hds6usi.execute-api.us-east-1.amazonaws.com/mocks');
-  await page.locator('#fetchBtn').click();
-  const successMessage = page.locator('#result'); 
-  await expect(successMessage).toHaveText('Success! Expected data received.');
-});
-
-test('check mocked success message', async ({ page }) => {
-    await page.goto('https://pu5hds6usi.execute-api.us-east-1.amazonaws.com/mocks');
-    // Мокируем успешный ответ до клика
-    await page.route('https://pu5hds6usi.execute-api.us-east-1.amazonaws.com/mocks', route => {
-      route.fulfill({
-        status: 200,
-        contentType: 'application/json',
-        body: JSON.stringify({
-          id: 1,
-          name: 'Test Mocked Item',
-          status: 'active',
-        }),
-      });
-    });
-    await page.locator('#fetchBtn').click();
-    const result = page.locator('#result');
-    // Ждём, пока UI покажет сообщение об успехе
-    await expect(result).toHaveText(/Success! Expected data received./);
+  test('check abort request → Network error', async ({ page }) => {
+    //моки всегда будут написаны в коде ДО самого запроса так как мы здесь говорим плэйрайту слушай запросы и как только услышишь замени на это
+    await page.route(API_Pattern, route => route.abort());
+    await page.goto(Base_Url);
+    await page.click('#fetchBtn');
+    await expect(page.locator('#result')).toHaveText('Network error');
+    await expect(page.locator('#result')).toHaveClass(/error/);
   });
 
-test('check mocked error message text and data', async ({ page }) => {
-  await page.goto('https://pu5hds6usi.execute-api.us-east-1.amazonaws.com/mocks');
-  // Мокируем ответ с ошибкой до клика
-  await page.route('https://pu5hds6usi.execute-api.us-east-1.amazonaws.com/mocks', route => {
+  test('check continue request → real backend call', async ({ page }) => {
+    await page.route(API_Pattern, route => route.continue());//перехватываем запрос, ничего с ним не делаем, отправляем дальше
+    await page.goto(Base_Url);
+    await page.click('#fetchBtn');
+    await expect(page.locator('#result')).not.toHaveText('Loading...');
+  });
+
+  test('check success 200', async ({ page }) => {
+    await page.goto(Base_Url);
+    await page.click('#fetchBtn');
+    const result = page.locator('#result');
+    await expect(result).toHaveText('Success! Expected data received.');
+    await expect(result).toHaveClass(/success/);//Регулярка проверит есть ли слово success где-то в списке классов
+  });
+
+  test('check success 201', async ({ page }) => {
+    await page.route(API_Pattern, route =>
+      route.fulfill({
+        status: 201,
+        contentType: 'application/json',
+        body: JSON.stringify({ message: 'Created' }),//JSON.stringify(...) нужен потому что HTTP-ответ всегда передаёт ТЕКСТ,а не JavaScript-объект.
+      })
+    );
+    await page.goto(Base_Url);
+    await page.click('#fetchBtn');
+    // status 201 не равен 200 и не >403 → else
+    await expect(page.locator('#result')).toHaveText('Status: 201');
+  });
+
+  test('check error 400', async ({ page }) => {
+    await page.route(API_Pattern, route =>
+      route.fulfill({
+        status: 400,
+        contentType: 'application/json',
+        body: JSON.stringify({ message: 'Bad Request' }),
+      })
+    );
+    await page.goto(Base_Url);
+    await page.click('#fetchBtn');
+    const result = page.locator('#result');
+    await expect(result).toHaveText('Status: 400');
+  });
+
+  test('check error 404', async ({ page }) => {
+    await page.route(API_Pattern, route =>
+      route.fulfill({
+        status: 404,
+        contentType: 'application/json',
+        body: JSON.stringify({ message: 'Not Found' }),
+      })
+    );
+    await page.goto(Base_Url);
+    await page.click('#fetchBtn');
+    const result = page.locator('#result');
+    await expect(result).toHaveText('Error 404: Not Found');
+    await expect(result).toHaveClass(/error/);
+  });
+
+  test('check error 500', async ({ page }) => {
+    await page.route(API_Pattern, route =>
       route.fulfill({
         status: 500,
         contentType: 'application/json',
-        body: JSON.stringify({ error: 'Network Error' }),
-      });
-    });
-  await page.locator('#fetchBtn').click();
-  const result = page.locator('#result');
-  // Проверяем, что сообщение об успехе не отображается
-  await expect(page.getByText('Success! Expected data received.')).not.toBeVisible();
+        body: JSON.stringify({ message: 'Internal Server Error' }),
+      })
+    );
+    await page.goto(Base_Url);
+    await page.click('#fetchBtn');
+    const result = page.locator('#result');
+    await expect(result).toHaveText(
+      'Error 500: Internal Server Error'
+    );
+    await expect(result).toHaveClass(/error/);
   });
 
- 
-
+});
